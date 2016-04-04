c10d35064bcdeefaeb6903b686dabd7820cf1f74
jrzapata <jordanrzapata@gmail.com>
2016-04-04 10:28:29 -0400

    init commit

---
+++ db/seeds.rb
@@ -0,0 +1,33 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+# jordan = Vendor.create(first_name: "Jordan", last_name: "Zapata", num_spots: 3)
+#
+# jzspot = Spot.create(address: "bladsfasdfasd", zipcode: "12311", available: true, price: 15, description: "blahadsfas asdf")
+#
+# jzspot2 = Spot.create(address: "bladsfasdfasd", zipcode: "12311", available: true, price: 15, description: "blahadsfas asdf")
+#
+# jzspot3 = Spot.create(address: "bladsfasdfasd", zipcode: "12311", available: true, price: 15, description: "blahadsfas asdf")
+#
+#
+# jordan.spots << jzspot
+#
+# jordan.spots << jzspot2
+#
+# jordan.spots << jzspot3
+#
+# sam = Customer.create(first_name: "Sam", last_name: "Langford", make: "Ford", model: "Mustang")
+#
+# sam.spots << jzspot
+#
+# jzspot.update(available: false)
+#
+# josh = Customer.create(first_name: "Josh", last_name: "Davis", make: "Toyota", model: "Camry")
+#
+# josh.spots << jzspot2
+#
+# jzspot2.update(available: false)
