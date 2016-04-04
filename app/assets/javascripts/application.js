1c61850808f49ce18818d6debd0599ccf05c4abf
jrzapata <jordanrzapata@gmail.com>
2016-04-04 13:06:51 -0400

    adding require tree for maps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require_tree .
 //= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require underscore-min.js
