c874a122f127c9d72daaaae7bf0c8183e9513c05
jrzapata <jordanrzapata@gmail.com>
2016-04-05 03:39:57 -0400

    fixed public assets bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => Rails.application.secrets.BUCKET,
+    :access_key_id => Rails.application.secrets.AKID,
+    :secret_access_key => Rails.application.secrets.SAK
+  }
+}
 
 
 end
