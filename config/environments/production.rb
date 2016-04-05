4a16464fc0ee63a64a8b853b5e9659838ac4a4cd
jrzapata <jordanrzapata@gmail.com>
2016-04-05 03:02:26 -0400

    fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Paperclip.options[:command_path] = "/opt/ImageMagick/bin/convert"
 
 
 end
